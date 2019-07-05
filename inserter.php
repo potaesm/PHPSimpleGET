398fcfd5e449d07597b3b9b15f415b2e1f0b6e4f
Suthinan Musitmani <suthinan.musitmani@gmail.com>
2019-07-05 14:26:07 +0700

    First Commit

--- inserter.php
+++ inserter.php
@@ -7,9 +7,9 @@
         $col1 = $_GET['col1'];
         $col2 = $_GET['col2'];
 
-        $obj->col1 = $col1;
-        $obj->col2 = $col2;
-        $obj->sum = $col1 + $col2;
+        $obj[0]->col1 = $col1;
+        $obj[0]->col2 = $col2;
+        $obj[1]->sum = $col1 + $col2;
 
         $jsonObj = json_encode($obj);
 
